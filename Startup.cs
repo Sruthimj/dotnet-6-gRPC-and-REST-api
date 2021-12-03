ea8ce081a8c0da02bbb4d843b13dcdf2b0b37f3c
Ryan Callahan <RyanCallahan312@gmail.com>
2021-12-03 01:51:47 -0500

    Inital Commit

---
+++ Startup.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using grpc_rest_api.Services;
+using System;
+using System.Threading;
+
+namespace grpc_rest_api
+{
+    public class Startup
+    {
+        public IConfiguration _config { get; }
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc(options => { options.EnableDetailedErrors = true; });
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "auto_highlighter_highlights_service", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "auto_highlighter_highlights_service v1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGrpcService<GreeterService>().RequireHost($"*:{_config["Grpc:HttpPort"]}", $"*:{_config["Grpc:HttpsPort"]}");
+            });
+        }
+    }
+}
